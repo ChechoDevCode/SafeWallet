cc7223ca07449d979c5c54962668c02ec20ba113
ChechoDevCode <sergio_gonzalez_17@hotmail.com>
2023-11-16 11:57:41 -0500

    Vista de gastos agregada

--- resources/views/vistaGastos.blade.php
+++ resources/views/vistaGastos.blade.php
@@ -1,3 +1,71 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<?php include("../src/seguridad.php"); ?>
+<?php include("../layouts/Header-dashboard.php"); ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+        <div id="contain">
+          <h2 class="text-analitycs py-2">Mis gastos</h2>
+          <section>
+            <table class="table p-4 m-4">
+              <thead>
+                <tr>
+                  <th>Descripción</th>
+                  <th>Valor</th>
+                  <th>Fecha</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope="row">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptates doloribus voluptate, repudiandae laboriosam nesciunt magni nostrum voluptatibus, inventore distinctio quia cumque ea quisquam culpa ipsum, cupiditate unde officiis doloremque!</td>
+                  <td data-label="Valor">$20.000.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+                <tr>
+                  <td scope="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatem cupiditate molestiae. Reprehenderit, quasi! Nobis nisi iusto omnis error corrupti eveniet, suscipit ex eligendi, cum repudiandae iste molestiae odit at?</td>
+                  <td data-label="Valor">$2.000.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+                  <td scope="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatem cupiditate molestiae. Reprehenderit, quasi! Nobis nisi iusto omnis error corrupti eveniet, suscipit ex eligendi, cum repudiandae iste molestiae odit at?</td>
+                  <td data-label="Valor">$10.000.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+                  <td scope="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatem cupiditate molestiae. Reprehenderit, quasi! Nobis nisi iusto omnis error corrupti eveniet, suscipit ex eligendi, cum repudiandae iste molestiae odit at?</td>
+                  <td data-label="Valor">$1.000.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+                  <td scope="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatem cupiditate molestiae. Reprehenderit, quasi! Nobis nisi iusto omnis error corrupti eveniet, suscipit ex eligendi, cum repudiandae iste molestiae odit at?</td>
+                  <td data-label="Valor">$500.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+                  <td scope="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatem cupiditate molestiae. Reprehenderit, quasi! Nobis nisi iusto omnis error corrupti eveniet, suscipit ex eligendi, cum repudiandae iste molestiae odit at?</td>
+                  <td data-label="Valor">$750.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+                  <td scope="row">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptatem cupiditate molestiae. Reprehenderit, quasi! Nobis nisi iusto omnis error corrupti eveniet, suscipit ex eligendi, cum repudiandae iste molestiae odit at?</td>
+                  <td data-label="Valor">$100.000.000</td>
+                  <td data-label="Fecha">19/05/2023</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
+        </div>    
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
